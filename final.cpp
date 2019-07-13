388f1b55f2db005cf8dab23704372504a2e40d11
KotaroKawase <52265677+KotaroKawase@users.noreply.github.com>
2019-07-13 12:03:31 +0900

    Add files via upload

--- final.cpp
+++ final.cpp
@@ -70,172 +70,172 @@ int main()
 	init_pair(3, COLOR_BLACK, COLOR_WHITE);		// 色3 は白地に黒文字
 	bkgd(COLOR_PAIR(1));
 
-	time_t t1, t2, t3;	//タイマー機能
+	time_t t1, t2;	//タイマー機能
 	while (1) {
 		TITLE:
-		Title();
-		mvaddstr(25, 40, "START -> Press [S] Button");
-		GetPrivateProfileString(section, keyWord, "none", keyValue, CHARBUFF, settingFile);
-		a = atoi(keyValue);
-		mvprintw(28, 40, "BestTime = %d", a);
-		while (1) {
-			key = getch();
-			if (key == 's') {
-				clear();
-
-				goto START;
-			}
-		}
-	START:
-		time(&t1);		//スタート時刻を記憶
-		iniLine();
-		while (1) {
-			t++;
-			timeout(0);
-			key = getch();
-
-			t2 = 0;
-			if (t % 100000 == 0) {
-				sx1 = w + 1;
-				sy1 = rand() % (h - 10);
-				Writingline1(sx1, sy1);
-			}
-			if (t % 100000 == 10000) {
-				sx2 = rand() % (w - 15);
-				sy2 = -1;
-				Writingline2(sx2, sy2);
-			}
-			if (t % 100000 == 0) {
-				ax = -1;
-				ay = rand() % w;
-				etr1 = "#";
-				mvaddstr(ax, ay, etr1);
-			}
-			if (t % 100000 == 20000) {
-				bx = -1;
-				by = rand() % w;
-				etr2 = "#";
-				mvaddstr(bx, by, etr2);
-			}
-			switch (key) {
-			case KEY_UP:	if (y > 0) { y--; erase(); }
-							break;		//キー操作のたびにt2を更新
-			case KEY_DOWN:	if (y < h - 1) { y++; erase(); }
-							break;
-			case KEY_LEFT:	if (x > 0) { x--;  erase(); }
-							break;
-			case KEY_RIGHT:	if (x < w - 1) {
-				x++;
-				erase();
-			}
-							break;
-			case 'a':		//[A]ボタンを押す取リトライ
-				check = 0;
-				str = "@";
-				bkgd(COLOR_PAIR(1));
-				y = h / 2;
-				x = w / 2;
-				time(&t2);
-				t1 = t2;
-				iniLine();
-				erase();
-				break;
-			}
-			if (t % 700 == 0) {
-				erase();
-				sx1--;
-
-			}
-			if (t % 3000 == 0 && t > 10000) {
-				erase();
-				sy2++;
-			}
-			if (t % 5000 == 0) {
-				erase();
-				ax++;
-			}
-			if (t % 5000 == 0) {
-				erase();
-				bx++;
-			}
-
-			mvaddstr(y, x, str);
-			mvprintw(1, 1, "count = %d", cnt);
-			refresh();
-			Writingline1(sx1, sy1);
-			Writingline2(sx2, sy2);
-			mvaddstr(ax, ay, etr1);
-			mvaddstr(bx, by, etr2);
-			if ((x == sx1 && (y <= sy1 || y > sy1 + 10)) || ((x <= sx2 || x > sx2 + 15) && y == sy2)) {
-				check = 1;
-
-			}
-			if ((x == ay && y == ax) && ((etr1 == "#"))) {
-				cnt++;
-				etr1 = " ";
-			}
-			else if ((x == by && y == bx) && (etr2 == "#")) {
-				cnt++;
-				etr2 = " ";
-			}
-
-			if (cnt == 3) {
-				check = 2;
-			}
-			if (check == 1) {
-				erase();
-				time(&t2);
-				goto OVER;
-			}
-			else if (check == 2) {
-				erase();
-				time(&t2);
-				goto CLEAR;
-			}
-		}
-	CLEAR:
-		while (1) {
-			str = " ";
-			bkgd(COLOR_PAIR(2));
-			disClear();
-			//ベストタイムだった場合にiniに書き込む
-			cleartime = t2 - t1;
-			char clear_data[CHARBUFF];
-			sprintf_s(clear_data, "%d", cleartime);
+			Title();
+			mvaddstr(25, 40, "START -> Press [S] Button");
 			GetPrivateProfileString(section, keyWord, "none", keyValue, CHARBUFF, settingFile);
 			a = atoi(keyValue);
-			if (cleartime < a || a == NULL) {
-				WritePrivateProfileString(section, keyWord, clear_data, settingFile);
-			}
+			mvprintw(28, 40, "BestTime = %d", a);
+			while (1) {
+				key = getch();
+				if (key == 's') {
+					clear();
 
-			mvprintw(25, 36, "time = %d", (t2 - t1));
-			mvaddstr(28, 36, "RETRY -> Press [A] Button");
-			check = 0;
-			key = getch();
-			if (key == 'a') {
-				bkgd(COLOR_PAIR(1));
-				str = "@";
-				cnt = 0;
-				goto TITLE;
+					goto START;
+				}
 			}
-		}
-	OVER:
-		while (1) {
-			str = " ";
-			bkgd(COLOR_PAIR(3));
-			disOver();
-			mvprintw(23, 36, "time = %d", (t2 - t1));
-			mvprintw(25, 36, "Count = %d", cnt);
-			mvaddstr(28, 36, "RETRY -> Press [A] Button");
-			check = 0;
-			key = getch();
-			if (key == 'a') {
-				bkgd(COLOR_PAIR(1));
-				str = "@";
-				cnt = 0;
-				goto TITLE;
+		START:
+			time(&t1);		//スタート時刻を記憶
+			iniLine();
+			while (1) {
+				t++;
+				timeout(0);
+				key = getch();
+
+				t2 = 0;
+				if (t % 100000 == 0) {
+					sx1 = w + 1;
+					sy1 = rand() % (h - 10);
+					Writingline1(sx1, sy1);
+				}
+				if (t % 100000 == 10000) {
+					sx2 = rand() % (w - 15);
+					sy2 = -1;
+					Writingline2(sx2, sy2);
+				}
+				if (t % 100000 == 0) {
+					ax = -1;
+					ay = rand() % w;
+					etr1 = "#";
+					mvaddstr(ax, ay, etr1);
+				}
+				if (t % 100000 == 20000) {
+					bx = -1;
+					by = rand() % w;
+					etr2 = "#";
+					mvaddstr(bx, by, etr2);
+				}
+				switch (key) {
+				case KEY_UP:	if (y > 0) { y--; erase(); }
+								break;		//キー操作のたびにt2を更新
+				case KEY_DOWN:	if (y < h - 1) { y++; erase(); }
+								break;
+				case KEY_LEFT:	if (x > 0) { x--;  erase(); }
+								break;
+				case KEY_RIGHT:	if (x < w - 1) {
+					x++;
+					erase();
+				}
+								break;
+				case 'a':		//[A]ボタンを押す取リトライ
+					check = 0;
+					str = "@";
+					bkgd(COLOR_PAIR(1));
+					y = h / 2;
+					x = w / 2;
+					time(&t2);
+					t1 = t2;
+					iniLine();
+					erase();
+					break;
+				}
+				if (t % 700 == 0) {
+					erase();
+					sx1--;
+
+				}
+				if (t % 3000 == 0 && t > 10000) {
+					erase();
+					sy2++;
+				}
+				if (t % 5000 == 0) {
+					erase();
+					ax++;
+				}
+				if (t % 5000 == 0) {
+					erase();
+					bx++;
+				}
+
+				mvaddstr(y, x, str);
+				mvprintw(1, 1, "count = %d", cnt);
+				refresh();
+				Writingline1(sx1, sy1);
+				Writingline2(sx2, sy2);
+				mvaddstr(ax, ay, etr1);
+				mvaddstr(bx, by, etr2);
+				if ((x == sx1 && (y <= sy1 || y > sy1 + 10)) || ((x <= sx2 || x > sx2 + 15) && y == sy2)) {
+					check = 1;
+
+				}
+				if ((x == ay && y == ax) && ((etr1 == "#"))) {
+					cnt++;
+					etr1 = " ";
+				}
+				else if ((x == by && y == bx) && (etr2 == "#")) {
+					cnt++;
+					etr2 = " ";
+				}
+
+				if (cnt == 3) {
+					check = 2;
+				}
+				if (check == 1) {
+					erase();
+					time(&t2);
+					goto OVER;
+				}
+				else if (check == 2) {
+					erase();
+					time(&t2);
+					goto CLEAR;
+				}
 			}
-		}
+		CLEAR:
+			while (1) {
+				str = " ";
+				bkgd(COLOR_PAIR(2));
+				disClear();
+				//ベストタイムだった場合にiniに書き込む
+				cleartime = (int)t2 - (int)t1;
+				char clear_data[CHARBUFF];
+				sprintf_s(clear_data, "%d", cleartime);
+				GetPrivateProfileString(section, keyWord, "none", keyValue, CHARBUFF, settingFile);
+				a = atoi(keyValue);
+				if (cleartime < a || a == NULL) {
+					WritePrivateProfileString(section, keyWord, clear_data, settingFile);
+				}
+
+				mvprintw(25, 36, "time = %d", (t2 - t1));
+				mvaddstr(28, 36, "RETRY -> Press [A] Button");
+				check = 0;
+				key = getch();
+				if (key == 'a') {
+					bkgd(COLOR_PAIR(1));
+					str = "@";
+					cnt = 0;
+					goto TITLE;
+				}
+			}
+		OVER:
+			while (1) {
+				str = " ";
+				bkgd(COLOR_PAIR(3));
+				disOver();
+				mvprintw(23, 36, "time = %d", (t2 - t1));
+				mvprintw(25, 36, "Count = %d", cnt);
+				mvaddstr(28, 36, "RETRY -> Press [A] Button");
+				check = 0;
+				key = getch();
+				if (key == 'a') {
+					bkgd(COLOR_PAIR(1));
+					str = "@";
+					cnt = 0;
+					goto TITLE;
+				}
+			}
 	
 	}
 	endwin();
